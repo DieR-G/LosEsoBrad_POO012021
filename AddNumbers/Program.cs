b7262c9a0f473bf42efc7236a197a14e3d74cda6
DieR-G <00043920@uca.edu.sv>
2021-03-11 18:28:39 -0600

    first commit

---
+++ AddNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"The sum {a} + {b} is {add(a,b)}");
+        }
+        public static int add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
